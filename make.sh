6e97e4e961f162fcbc347ed5b8cbf366d23edcfb
Daniel Hidalgo Chica <daniel5000.dhc@gmail.com>
2024-12-28 15:37:19 +0100

    First commit

---
+++ make.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#########################
+# .make.sh
+# Tis script creates symlinks from the home dir to any
+# desired dotfiles in ~/dotfiles
+#########################
+
+
+# Varibales
+
+
+dir=~/dotfiles          # dotfiles dir
+olddir=~/dotfiles_old   # odl dotfiles backup
+
+files="bashrc bash_profile vimrc vim xprofile" # List of files
+                                      	       # to symlink in home
+
+# create dotfiles_old in homedir
+
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir 
+echo "...done"
+
+# change to the dotfiles dir
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory
+# then create symlinks
+
+for file in $files; do
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file $olddir
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/$file ~/.$file
+done
+
+
+# Specific one for i3 config
+i3config_folder=~/.config/i3
+i3config_file=config
+
+ln -s $dir/$i3config_file $i3config_folder
